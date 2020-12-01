ce71f8296e531451946739c9cf19f535c3dcbabc
Christian Eriksson <christian.eriksson@live.se>
2020-12-01 22:45:37 +0100

    Add missing trim_to_first_right_slash function

--- string_utils.sh
+++ string_utils.sh
@@ -10,3 +10,19 @@ trim_left_slash() {
     echo ${string#/}
 }
 
+trim_to_first_right_slash() {
+    string="$*"
+    while :; do
+        case $string in
+            */)
+                break;;
+            *)
+                if [ -z $string ]; then
+                    break
+                fi
+                string=${string%?};;
+        esac
+    done
+    echo $string
+}
+
