f4358a6bd4fc12510e1adc8f749f4cd3229bff7c
Christian Eriksson <christian.eriksson@live.se>
2020-12-06 21:58:18 +0100

    Format test result output.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,9 +12,15 @@ test_scripts="
 
 for test_script in $test_scripts; do
     results=$(eval $test_script | grep -e "^PASS$" -e "^FAIL$")
-    echo "RESULTS:"
-    echo "$test_script"
+    echo "\033[1mRESULTS:\033[m"
+    echo "\033[1m$test_script\033[m"
     for result in $results; do
-        echo $result
+        if [ "$result" = "PASS" ]; then
+            echo "\033[1m\033[38;5;2m$result\033[m"
+        fi
+
+        if [ "$result" = "FAIL" ]; then
+            echo "\033[1m\033[38;5;9m$result\033[m"
+        fi
     done
 done
