4c014975a520b0b0d4e42c5aa69460d1129c2342
Christian Eriksson <christian.eriksson@live.se>
2020-12-06 15:28:51 +0100

    Test consecutive backups with single backup config.

---
+++ tests/create_backups.tests/consecutive_backups.test.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+script_name="consecutive_backups.test.sh"
+relative_dir="${0%/*}"
+cd $relative_dir
+
+. ../test_utils.sh
+
+test_dir="$(pwd)"
+backup_json="consecutive_backups.backup.json"
+backup_dir="$test_dir/consecutive_backups.result"
+root="$test_dir/test_files_root"
+root_copy="$test_dir/test_files_root.copy"
+cp -r $root $root_copy
+
+# given a test json
+name=consecutive
+echo "\
+{
+    \"backup_destination\": \"$backup_dir\",
+    \"backup_configs\": [
+        {
+            \"name\": \"$name\",
+            \"root\": \"$root_copy\",
+            \"paths\": [
+                \"file1\",
+                \"dir1/file3\",
+                \"dir1/sub_dir\",
+                \"dir2\"
+            ]
+        }
+    ]
+}\
+" > $backup_json
+
+# change to blubee root path
+cd ../..
+
+# when we run blubee
+blubee -b "$test_dir/$backup_json" backup
+
+# and we change a few files
+files="file1 dir1/sub_dir/file7 dir2/file5"
+for file in $files; do
+    echo "updated once" > $root_copy/$file
+done
+
+# and we wait for a bit
+sleep 1
+
+# and we run blubee
+blubee -b "$test_dir/$backup_json" backup
+
+result_dir="$backup_dir/$name"
+test_restults=""
+
+# then there are three files/directories in the backup directory
+file_count=$(count $result_dir/*)
+[ $file_count -eq 3 ] \
+    && test_results="$test_results PASS" \
+    || test_results="$test_results FAIL"
+
+# and two of the directories has a name similar to a date stamp
+date_dir_count=$(find $result_dir -maxdepth 1 -type d | grep -e "[0-9]\{8\}_[0-9]\{6\}$" | wc -l)
+[ $date_dir_count -eq 2 ] \
+    && test_results="$test_results PASS" \
+    || test_results="$test_results FAIL"
+
+# and the latest backup is a link
+[ -L "$result_dir/latest" ] \
+    && test_results="$test_results PASS" \
+    || test_results="$test_results FAIL"
+
+# and the latest backup has the expected content
+diff -r "$result_dir/latest" "$test_dir/consecutive_backups.expected" \
+    && test_results="$test_results PASS" \
+    || test_results="$test_results FAIL"
+
+echo "$script_name\nRESULTS:"
+for result in $test_results; do
+    echo $result
+done
+
+# clean up
+rm -r "$backup_dir"
+rm "$test_dir/$backup_json"
+rm -r $root_copy
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,10 +4,16 @@ relative_dir="${0%/*}"
 cd $relative_dir
 
 # create backups
-test_script="./create_backups.tests/simple_backup.test.sh"
-results=$(eval $test_script | grep -e "^PASS$" -e "^FAIL$")
-echo "RESULTS:"
-echo "$test_script"
-for result in $results; do
-    echo $result
+test_scripts="
+./create_backups.tests/simple_backup.test.sh
+./create_backups.tests/consecutive_backups.test.sh
+"
+
+for test_script in $test_scripts; do
+    results=$(eval $test_script | grep -e "^PASS$" -e "^FAIL$")
+    echo "RESULTS:"
+    echo "$test_script"
+    for result in $results; do
+        echo $result
+    done
 done
