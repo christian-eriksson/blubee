6f1e0094537655b0d9f3a692381ca56352e668fc
Christian Eriksson <christian.eriksson@live.se>
2020-12-06 23:21:25 +0100

    Print summary of result at the end of test run

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,6 +10,8 @@ test_scripts="
 ./restore_backups.tests/simple_restore.test.sh
 "
 
+passed=0
+failed=0
 for test_script in $test_scripts; do
     results=$(eval $test_script 2> /dev/null | grep -e "^PASS$" -e "^FAIL$")
     echo "\033[1mRESULTS:\033[m"
@@ -17,10 +19,17 @@ for test_script in $test_scripts; do
     for result in $results; do
         if [ "$result" = "PASS" ]; then
             echo "\033[1m\033[38;5;2m$result\033[m"
+            passed=$((passed + 1))
         fi
 
         if [ "$result" = "FAIL" ]; then
             echo "\033[1m\033[38;5;9m$result\033[m"
+            failed=$((failed + 1))
         fi
     done
 done
+
+echo "\n\033[1mRESULTS:\033[m"
+echo "\033[1m\033[38;5;2m$passed tests passed\033[m"
+echo "\033[1m\033[38;5;9m$failed tests failed\033[m"
+
