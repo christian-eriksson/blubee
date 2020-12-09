173059ef00ecc5217245e8132ea66fc4959e9a1e
Christian Eriksson <christian.eriksson@live.se>
2020-12-10 00:50:11 +0100

    Rename test simple backup to single backup

--- tests/create_backups.tests/single_backup.test.sh
+++ tests/create_backups.tests/single_backup.test.sh
@@ -6,13 +6,13 @@ cd $relative_dir
 . ../test_utils.sh
 
 test_dir="$(pwd)"
-backup_json="simple_backup.backup.json"
+backup_json="single_backup.backup.json"
 
 # GIVEN a test json
 name=simple
 echo "\
 {
-    \"backup_destination\": \"$test_dir/simple_backup.result\",
+    \"backup_destination\": \"$test_dir/single_backup.result\",
     \"backup_configs\": [
         {
             \"name\": \"$name\",
@@ -32,7 +32,7 @@ echo "\
 cd ../..
 ./blubee -b "$test_dir/$backup_json" backup
 
-result_dir="$test_dir/simple_backup.result/$name"
+result_dir="$test_dir/single_backup.result/$name"
 test_results=""
 
 # THEN there are two files/directories in the backup directory
@@ -45,13 +45,13 @@ test_results="$test_results $(assert_datetime_dir_count "$result_dir" 1)"
 test_results="$test_results $(assert_is_link "$result_dir/latest")"
 
 # and the latest backup has the expected content
-has_same_content=$(assert_dirs_equal "$result_dir/latest" "$test_dir/simple_backup.expected")
+has_same_content=$(assert_dirs_equal "$result_dir/latest" "$test_dir/single_backup.expected")
 test_results="$test_results $has_same_content" 
 
-echo "simple_backup.test.sh\nRESULTS:"
+echo "single_backup.test.sh\nRESULTS:"
 echo "$(asserts_to_text "$test_results")"
 
 # clean up
-rm -r "$test_dir/simple_backup.result"
+rm -r "$test_dir/single_backup.result"
 rm "$test_dir/$backup_json"
 
