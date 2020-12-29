40142e8843c08dc5cea2cde247a10f7a8b54c298
Christian Eriksson <christian.eriksson@live.se>
2020-12-29 02:40:50 +0100

    Implement launch script
    The launch script should be used when installing blubee as an application.
    With the launch script present in the PATH (preferably under the name
    blubee) it will expect the blubee script and config files to be in
    /etc/blubee

---
+++ tests/blubee_launcher.tests/passing_args_to_blubee.test.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+relative_dir="${0%/*}"
+cd $relative_dir
+test_dir="$(pwd)"
+cd ..
+test_root="$(pwd)"
+cd $test_dir
+
+. ../test_utils.sh
+activate_blubee_mock ../../launcher $test_dir/launcher
+
+# GIVEN a test json path
+backup_json="some/backup/json"
+
+# AND a config path
+config_path="a/config/path/"
+
+# AND some dummy option
+dummy_option="-8 dummy"
+
+# AND some command
+command="some_command"
+
+# AND some dummy command
+dummy_command="dummy"
+
+# WHEN we run blubee with config and backup
+output=$(./launcher -c "$config_path" -b "$backup_json")
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the expected arguments to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c $config_path" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+backup_json_option_count=$(echo "$output" | grep -e "blubee.*-b $backup_json" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$backup_json_option_count" 1)"
+
+dummy_option_count=$(echo "$output" | grep -e "blubee.*$dummy_option" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$dummy_option_count" 0)"
+
+# WHEN we run blubee with only config
+output=$(./launcher -c "$config_path")
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$test_results $(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the expected arguments to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c $config_path" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+backup_json_option_count=$(echo "$output" | grep -e "blubee.*-b $backup_json" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$backup_json_option_count" 0)"
+
+dummy_option_count=$(echo "$output" | grep -e "blubee.*$dummy_option" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$dummy_option_count" 0)"
+
+# WHEN we run blubee with only config and dummy option
+output=$(./launcher -c "$config_path" $dummy_option)
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$test_results $(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the expected arguments to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c $config_path" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+backup_json_option_count=$(echo "$output" | grep -e "blubee.*-b $backup_json" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$backup_json_option_count" 0)"
+
+dummy_option_count=$(echo "$output" | grep -e "blubee.*$dummy_option" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$dummy_option_count" 1)"
+
+# WHEN we run blubee with a command
+output=$(./launcher -c "$config_path" $command)
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$test_results $(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the expected arguments to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c $config_path" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+command_count=$(echo "$output" | grep -e "blubee.*$command" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$command_count" 1)"
+
+dummy_command_count=$(echo "$output" | grep -e "blubee.*$dummy_command" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$dummy_command_count" 0)"
+
+# WHEN we run blubee with two commands
+output=$(./launcher -c "$config_path" $dummy_command $command)
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$test_results $(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the expected arguments to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c $config_path" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+command_count=$(echo "$output" | grep -e "blubee.*$command" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$command_count" 1)"
+
+dummy_command_count=$(echo "$output" | grep -e "blubee.*$dummy_command" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$dummy_command_count" 1)"
+echo "passing_args_to_blubee.test.sh.test.sh\nRESULTS:"
+echo "$(asserts_to_text "$test_results")"
+
+rm $test_dir/launcher
---
+++ tests/blubee_launcher.tests/passing_no_config_to_launcher.test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+relative_dir="${0%/*}"
+cd $relative_dir
+test_dir="$(pwd)"
+cd ..
+test_root="$(pwd)"
+cd $test_dir
+
+. ../test_utils.sh
+activate_blubee_mock ../../launcher $test_dir/launcher
+
+# GIVEN a test json path
+backup_json="some/backup/json"
+
+# AND some command
+command="some_command"
+
+# WHEN we run blubee without config
+output=$(./launcher -b "$backup_json" $command)
+exit_code="$?"
+
+# THEN launcher ran without crashing
+test_results="$(assert_equal_numbers "$exit_code" 0)"
+
+# AND launcher passed the default config to blubee
+config_option_count=$(echo "$output" | grep -e "blubee.*-c /etc/blubee/blubee.conf" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$config_option_count" 1)"
+
+# AND the other options are passed as expected
+backup_json_option_count=$(echo "$output" | grep -e "blubee.*-b $backup_json" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$backup_json_option_count" 1)"
+
+command_count=$(echo "$output" | grep -e "blubee.*$command" | wc -l)
+test_results="$test_results $(assert_equal_numbers "$command_count" 1)"
+
+echo "passing_args_to_blubee.test.sh.test.sh\nRESULTS:"
+echo "$(asserts_to_text "$test_results")"
+
+rm $test_dir/launcher
