a0b7962e2fae3b74d9e92844b59a93c844e6655e
Christian Eriksson <christian.eriksson@live.se>
2022-01-01 21:42:47 +0100

    Enable backslash escape in test runner
    The test runner (run_tests.sh) didn't print test results with backslash
    escapes enables (echo -e). This meant that the output of the test
    result was not properly formatted, depending on your shell.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,23 +10,23 @@ passed=0
 failed=0
 for test_script in $test_scripts; do
     results=$(eval $test_script 2> /dev/null | grep -e "^PASS$" -e "^FAIL$")
-    echo "\033[1mRESULTS:\033[m"
-    echo "\033[1m$test_script\033[m"
+    echo -e "\033[1mRESULTS:\033[m"
+    echo -e "\033[1m$test_script\033[m"
     for result in $results; do
         if [ "$result" = "PASS" ]; then
-            echo "\033[1m\033[38;5;2m$result\033[m"
+            echo -e "\033[1m\033[38;5;2m$result\033[m"
             passed=$((passed + 1))
         fi
 
         if [ "$result" = "FAIL" ]; then
-            echo "\033[1m\033[38;5;9m$result\033[m"
+            echo -e "\033[1m\033[38;5;9m$result\033[m"
             failed=$((failed + 1))
         fi
     done
-    echo ""
+    echo -e ""
 done
 
-echo "\033[1mRESULT SUMMARY:\033[m"
-echo "\033[1m\033[38;5;2m$passed tests passed\033[m"
-echo "\033[1m\033[38;5;9m$failed tests failed\033[m"
+echo -e "\033[1mRESULT SUMMARY:\033[m"
+echo -e "\033[1m\033[38;5;2m$passed tests passed\033[m"
+echo -e "\033[1m\033[38;5;9m$failed tests failed\033[m"
 
